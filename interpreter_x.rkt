65ab336e887c3fe50b9fc9d17aa8d13128c433e5
Saiful Abu <saifulabu25@gmail.com>
2017-12-25 21:04:36 -0600

    Update interpreter_x.rkt

--- interpreter_x.rkt
+++ interpreter_x.rkt
@@ -15,12 +15,9 @@
 (struct aunit ()    #:transparent) ;; unit value -- good for ending a list
 (struct isaunit (e) #:transparent) ;; evaluate to 1 if e is unit else 0
 
-;; a closure is not in "source" programs but /is/ a MUPL value; it is what functions evaluate to
+;; a closure is not in "source" programs but /is/ a X value; it is what functions evaluate to
 (struct closure (env fun) #:transparent) 
 
-;; Problem 1
-
-;; CHANGE (put your solutions here)
 (define (mupllist->racketlist m-list)
   (if (aunit? m-list)
        null
@@ -30,18 +27,14 @@
   (if (null? r-list)
        (aunit)
        (apair (car r-list) (racketlist->mupllist (cdr r-list)))))
- 
-;; Problem 2
 
 ;; lookup a variable in an environment
-;; Do NOT change this function
 (define (envlookup env str)
   (cond [(null? env) (error "unbound variable during evaluation" str)]
         [(equal? (car (car env)) str) (cdr (car env))]
         [#t (envlookup (cdr env) str)]))
 
-;; Do NOT change the two cases given to you.  
-;; DO add more cases for other kinds of MUPL expressions.
+;; DO add more cases for other kinds of X expressions.
 ;; We will test eval-under-env by calling it directly even though
 ;; "in real life" it would be a helper function of eval-exp.
 (define (eval-under-env e env)
@@ -54,8 +47,7 @@
                     (int? v2))
                (int (+ (int-num v1) 
                        (int-num v2)))
-               (error "MUPL addition applied to non-number")))]
-        ;; CHANGE add more cases here
+               (error "X addition applied to non-number")))]
         [(int? e) e]
         [(apair? e)
          (apair (eval-under-env (apair-e1 e) env) (eval-under-env (apair-e2 e) env))]
@@ -73,17 +65,17 @@
                (if (> (int-num v1) (int-num v2))
                    (eval-under-env (ifgreater-e3 e) env)
                    (eval-under-env (ifgreater-e4 e) env))
-               (error "MUPL ifgreater applied to non-number")))]
+               (error "X ifgreater applied to non-number")))]
         [(fst? e) 
          (let ([v (eval-under-env (fst-e e) env)])
            (if (apair? v)
                (apair-e1 v)
-               (error "MUPL fst was not applied to apair")))]
+               (error "X fst was not applied to apair")))]
         [(snd? e) 
          (let ([v (eval-under-env (snd-e e) env)])
            (if (apair? v)
                (apair-e2 v)
-               (error "MUPL snd was not applied to apair")))]
+               (error "X snd was not applied to apair")))]
 
         [(fun? e) (closure env e)]
 
@@ -107,16 +99,13 @@
                      (let ([fun-env (cons (cons fun-name clsr) fun-env)])
                        (eval-under-env (fun-body (closure-fun clsr)) fun-env))
                      (eval-under-env (fun-body (closure-fun clsr)) fun-env)))
-               (error "MUPL call was not applied to closure")))]
+               (error "X call was not applied to closure")))]
   
         [#t (error (format "bad MUPL expression: ~v" e))]))
 
-;; Do NOT change
 (define (eval-exp e)
   (eval-under-env e null))
         
-;; Problem 3
-
 (define (ifaunit e1 e2 e3)
   (ifgreater (isaunit e1) (int 0) e2 e3))
 
@@ -130,8 +119,6 @@
          (ifgreater (var "_x") (var "_y") e4
                     (ifgreater (var "_y") (var "_x") e4 e3))))
 
-;; Problem 4
-
 (define mupl-map
   (fun #f "mapfn"
        (fun "mapper" "lst"
@@ -143,20 +130,3 @@
   (mlet "map" mupl-map
         (fun #f "i"
              (call (var "map") (fun #f "x" (add (var "x") (var "i")))))))
-
-;; Challenge Problem
-
-(struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
-
-;; We will test this function directly, so it must do
-;; as described in the assignment
-(define (compute-free-vars e) "CHANGE")
-
-;; Do NOT share code with eval-under-env because that will make
-;; auto-grading and peer assessment more difficult, so
-;; copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
-
-;; Do NOT change this
-(define (eval-exp-c e)
-  (eval-under-env-c (compute-free-vars e) null))
